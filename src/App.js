40aff3eba3f170c9b4711531b991b3a829bdbf24
AnastasiaPanko <anastasiaa.pankoo@gmail.com>
2023-06-01 13:35:00 +0300

    create navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../src/Pages/Home';
+import Contacts from '../src/Pages/Contacts';
+import About from '../src/Pages/About';
+import Blog from '../src/Pages/Blog';
 
 function App() {
-    return <Header></Header>;
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contacts' element={<Contacts />} />
+                <Route path='/blog' element={<Blog />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,58 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from './assets/logo1.svg';
+import {
+    Navbar,
+    Nav,
+    Container,
+    Form,
+    FormControl,
+    Button,
+} from 'react-bootstrap';
+import logo from './logo.svg';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default class Header extends Component {
     render() {
         return (
-            <Navbar
-                fixed='top'
-                collapseOnSelect
-                expand='lg'
-                bg='light'
-                variant='light'
-            >
-                <Container>
-                    <Navbar.Brand href='/'>
-                        <img
-                            src={logo}
-                            height='30'
-                            width='30'
-                            className='d-inline-block align-top'
-                            alt='Logo'
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-                    <Navbar.Collapse>
-                        <Nav className='me-auto'>
-                            <Nav.Link href='/'> Home </Nav.Link>
-                            <Nav.Link href='/about'> About us </Nav.Link>
-                            <Nav.Link href='/contacts'> Contacts </Nav.Link>
-                            <Nav.Link href='/blog'> Blog </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <>
+                <Navbar collapseOnSelect expand='sm' bg='light' variant='light'>
+                    <Container>
+                        <Navbar.Brand href='/'>
+                            <img
+                                src={logo}
+                                height='40'
+                                width='40'
+                                className='d-inline-block align-top'
+                                alt='Logo'
+                            />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                        <Navbar.Collapse>
+                            <Nav className='me-auto'>
+                                <LinkContainer to='/'>
+                                    <Nav.Link> Home </Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to='/about'>
+                                    <Nav.Link> About us </Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to='/contacts'>
+                                    <Nav.Link> Contacts </Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to='/blog'>
+                                    <Nav.Link> Blog </Nav.Link>
+                                </LinkContainer>
+                            </Nav>
+                            <Form className='d-flex'>
+                                <FormControl
+                                    type='text'
+                                    placeholder='Search'
+                                    className='me-sm-3'
+                                />
+                                <Button variant='outline-info'>Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
         );
     }
 }
